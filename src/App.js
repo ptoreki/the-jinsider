1a256da2209cf139e5834e7b9e9c494c18a9a27d
Patrick Toreki <ptorekigit@gmail.com>
2022-07-04 13:16:20 +0200

    Deploy my project to github pages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The Jinsider</h1>
+      <h2>Author: Patrick J.Tøreki</h2>
+    </div>
+  );
+}
+
+export default App;
