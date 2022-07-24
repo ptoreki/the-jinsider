5b79ee3f3b963da27464994aba4795c3adc5939b
Patrick Toreki <ptorekigit@gmail.com>
2022-07-24 21:45:04 +0200

    Updates

--- src/client.js
+++
@@ -1,7 +0,0 @@
-import * as contentful from 'contentful'
-
-export const client = contentful.createClient({
-    space: process.env.REACT_APP_CONTENTFUL_SPACE_ID, 
-    accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN
-})
-
